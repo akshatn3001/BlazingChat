bbc88cdc658da621379f02f12ca2edcc9592509f
Akshat Narang <anarang@pageupsoft.com>
2024-04-16 18:16:49 +0530

    Add project files.

---
+++ BlazingChat/Server/Data/Migrations/20240416110236_firstdb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazingChat.Server.Data.Migrations
+{
+    public partial class firstdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
